0e28b7bd1dab30b054389bd9f15fd37c0fbf28cd
Greg Brunk <greg.brunk@gmail.com>
2016-07-26 15:09:58 -0600

    makes show route for individual album

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -1,6 +1,10 @@
 class RecordsController < ApplicationController
 	def index
-	  @records = Record.all
-	  render :index # optional
+		@records = Record.all
+		render :index
 	end
+	def show
+	    @record = Record.find(params[:id])
+	    render :show
+  	end
 end
--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-	resources :records, only: [:index]
+	resources :records, only: [:index, :show]
 end
