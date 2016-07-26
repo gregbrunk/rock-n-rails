aba457a3e7ee0f218517dece877b3c86e7fb3f05
Greg Brunk <greg.brunk@gmail.com>
2016-07-26 15:04:51 -0600

    makes basic index route, seeds database

--- records/db/seeds.rb
+++ records/db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Wipe the database
+Record.destroy_all
+# Let's create a bunch of records
+Record.create([
+  {
+    title: "Piper at the Gates of Dawn",
+    artist: "Pink Floyd",
+    year: 1967,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/3/3c/PinkFloyd-album-piperatthegatesofdawn_300.jpg",
+    song_count: 11
+  },
+  {
+    title: "The Dark Side of the Moon",
+    artist: "Pink Floyd",
+    year: 1973,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+    song_count: 10
+  },
+  {
+    title: "Wish You Were Here",
+    artist: "Pink Floyd",
+    year: 1975,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/a/a4/Pink_Floyd%2C_Wish_You_Were_Here_%281975%29.png",
+    song_count: 5
+  }
+])
